5472ce099fd7a1adf2670b797f00d428a4dc1540
lipeeeee <filipe.pereirafga@gmail.com>
2023-12-25 19:09:04 +0000

    Make skip list function

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go run srch/srch.go
--- srch/srch.go
+++ srch/srch.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("SRCH---ENTRY")
+  fmt.Println(makeSkipList("hello"))
 }
+
+func makeSkipList(pattern string) map[string]int {
+  skip_list := make(map[string]int)
+  pattern_length := len(pattern)
+  for index, char := range pattern {
+    skip_list[string(char)] = max(1, pattern_length - index - 1)
+  }
+  return skip_list
+}
+
