aef132db87ab1dda0786a8ebe0fb7815686baf5f
lipeeeee <filipe.pereirafga@gmail.com>
2023-12-28 09:51:16 +0000

    New flow defined in srch.go header, needs refactoring

--- src/srch.go
+++ src/srch.go
@@ -5,7 +5,7 @@ import (
 	//"bufio"
 	"errors"
 	"fmt"
-	"io/fs"
+	//"io/fs"
 	"log"
 	"os"
 
@@ -13,97 +13,108 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+/*
+  main
+  main processing / arg parsing
+  srch(path, recursive_flag)
+    makeSkipList
+    search files
+      for each file in lines
+        search & print found
+*/
+
 func main() {
-  files := getAllFilesInDirectory("./", true)
-  for idx, file := range files {
-    fmt.Println(fmt.Sprintf("%d:%h:%d", idx, file.Name(), file.IsDir()))
-  }
+	files := getAllFilesInDirectory("./", true)
+	for idx, file := range files {
+		fmt.Println(fmt.Sprintf("%d:%s", idx, file))
+	}
 
-  app := &cli.App {
-    Name:   "srch",
-    Usage:  "Performant recursive file content search tool",
-    Action: func(cCtx *cli.Context) error {
-      // Check number of args passed in before doing anything
-      // 0      -> provide breaf usage explanation
-      // not 2  -> return an error
-      switch cCtx.Args().Len() {
-      case 0:
-        fmt.Println(`USAGE: srch [OPTION] PATTERN [DIRECTORY/FILE]
+	app := &cli.App{
+		Name:  "srch",
+		Usage: "Performant recursive file content search tool",
+		Action: func(cCtx *cli.Context) error {
+			// Check number of args passed in before doing anything
+			// 0      -> provide breaf usage explanation
+			// not 2  -> return an error
+			switch cCtx.Args().Len() {
+			case 0:
+				fmt.Println(`USAGE: srch [OPTION] PATTERN [DIRECTORY/FILE]
 Try 'srch --help' for more information`)
-        return nil
-      case 2:
-        break
-      default:
-        return errors.New(fmt.Sprintf("%s%d",
-                          "Expected 2 arguments... Got ", cCtx.Args().Len()))
-      }
+				return nil
+			case 2:
+				break
+			default:
+				return errors.New(fmt.Sprintf("%s%d",
+					"Expected 2 arguments... Got ", cCtx.Args().Len()))
+			}
 
-      // Get complete path to search
-      path, err := getCompletePath(cCtx.Args().Get(1))
-      if err != nil {
-        return err
-      }
-      is_directory, err := isDirectory(path)
-      if err != nil {
-        return err
-      }
+			// Get complete path to search
+			path, err := getCompletePath(cCtx.Args().Get(1))
+			if err != nil {
+				return err
+			}
+			is_directory, err := isDirectory(path)
+			if err != nil {
+				return err
+			}
 
-      fmt.Println("path:", path, is_directory)
-      return nil
-    },
-  }
+			fmt.Println("path:", path, is_directory)
+			return nil
+		},
+	}
 
-  // Print any erorrs thrown 
-  if err := app.Run(os.Args); err != nil {
-    fmt.Println(err.Error())
-  }
+	// Print any erorrs thrown
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 // Gets complete path given a relative in cli execution
 func getCompletePath(relative_path string) (string, error) {
-  working_dir, err := os.Getwd()
-  if err != nil {
-    return "", err
-  }
-  return working_dir + "/" + relative_path, nil
+	working_dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return working_dir + "/" + relative_path, nil
 }
 
 // Checks if a path is a directory
 func isDirectory(path string) (bool, error) {
-  fileInfo, err := os.Stat(path)
-  if err != nil {
-    return false, err
-  }
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
 
-  return fileInfo.IsDir(), err
+	return fileInfo.IsDir(), err
 }
 
 // Get files in directory
-func getAllFilesInDirectory(path string, recursive bool) []fs.DirEntry {
+func getAllFilesInDirectory(path string, recursive bool) []string {
+	var c []string
 	entries, err := os.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, e := range entries {
-    if recursive && e.IsDir() {
-      recursive_dir_call := getAllFilesInDirectory(path + "/" + e.Name(), recursive)
-      for _, entry := range recursive_dir_call {
-        entries = append(entries, entry)
-      }
-    }
+		fmt.Println(e.Name())
+		/*if recursive && e.IsDir() {
+			recursive_dir_call := getAllFilesInDirectory(path+"/"+e.Name(), recursive)
+			for _, entry := range recursive_dir_call {
+				entries = append(entries, entry)
+			}
+		}*/
 	}
-  
-  return entries
+
+	return c
 }
 
-// Make boyer-moore skip dictionary 
+// Make boyer-moore skip dictionary
 func makeSkipList(pattern string) map[string]int {
-  skip_list := make(map[string]int)
-  pattern_length := len(pattern)
-  for index, char := range pattern {
-    skip_list[string(char)] = max(1, pattern_length - index - 1)
-  }
-  return skip_list
+	skip_list := make(map[string]int)
+	pattern_length := len(pattern)
+	for index, char := range pattern {
+		skip_list[string(char)] = max(1, pattern_length-index-1)
+	}
+	return skip_list
 }
-
